9ef9949645e7a32329e1fafad39a46a842ee8659
Alex Stéphane NGOMBOL <34063564+tikitonp@users.noreply.github.com>
2020-02-25 11:57:45 +0100

    Improve domains show pages with messages

--- resources/views/admin/domain/show.blade.php
+++ resources/views/admin/domain/show.blade.php
@@ -12,13 +12,76 @@
         <div class="col-12">
             <div class="card card-default">
                 <div class="card-header card-header-border-bottom d-flex justify-content-between">
-                    <h2>Détail du domaine</h2>
+                    <h2>Détail du domaine <strong>{{ $domain->name }}</strong></h2>
                 </div>
                 <!-- body -->
                 <div class="card-body">
-
+                    <div class="mt-2">
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="general-tab" data-toggle="tab" href="#general" role="tab" aria-controls="general" aria-selected="true">
+                                    <i class="mdi mdi-star mr-1"></i> Général</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="contributors-tab" data-toggle="tab" href="#contributors" role="tab" aria-controls="contributors"
+                                   aria-selected="false">
+                                    <i class="mdi mdi-account-group-outline mr-1"></i> Intervenants</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="documents-tab" data-toggle="tab" href="#documents" role="tab" aria-controls="documents"
+                                   aria-selected="false">
+                                    <i class="mdi mdi-file-document-box-multiple-outline mr-1"></i> Documents</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="contacts-tab" data-toggle="tab" href="#contacts" role="tab" aria-controls="contacts"
+                                   aria-selected="false">
+                                    <i class="mdi mdi-email-open-outline mr-1"></i> Messages</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane pt-3 fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
+                                <ul>
+                                    <li><i class="{{ $domain->icon }}"></i></li>
+                                    <li>{{ $domain->country->fr_name }}</li>
+                                    <li>{{ $domain->description }}</li>
+                                </ul>
+                                <p class="text-right">
+                                    <a class="btn btn-warning btn-sm text-white" title="Modifier"
+                                       href="{{ route('admin.domains.edit', [$domain]) }}">
+                                        <i class="mdi mdi-square-edit-outline"></i>
+                                    </a>
+                                    <button class="btn btn-danger btn-sm" data-toggle="modal" title="Supprimer"
+                                            data-target="#delete-modal">
+                                        <i class="mdi mdi-trash-can-outline"></i>
+                                    </button>
+                                </p>
+                            </div>
+                            <div class="tab-pane pt-3 fade" id="contributors" role="tabpanel" aria-labelledby="contributors-tab">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </div>
+                            <div class="tab-pane pt-3 fade" id="documents" role="tabpanel" aria-labelledby="documents-tab">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </div>
+                            <div class="tab-pane pt-3 fade" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
+                                @include('partials.admin.contacts-table', ['contacts' => $domain->contacts, 'domain_page' => true])
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    @component('components.delete-modal', [
+        'id' => 'delete-modal',
+        'title' => 'Supprimer ' . $domain->name,
+        'message' => 'Vous ne pourrez plus consulter ce domaine, êtes vous sûr?',
+        'route' => route('admin.domains.destroy', [$domain])
+    ])
+    @endcomponent
 @endsection
+
+@include('partials.table-page-push')
---
+++ resources/views/partials/admin/contacts-table.blade.php
@@ -0,0 +1,48 @@
+<div class="responsive-data-table">
+    <table id="responsive-data-table" class="table dt-responsive nowrap table-bordered table-hover" style="width:100%">
+        <thead class="bg-primary">
+        <tr>
+            <th class="text-white">Nom</th>
+            <th class="text-white">Email</th>
+            <th class="text-white">Sujet</th>
+            @if(!($domain_page ?? false))<th class="text-white">Domaine</th>@endif
+            <th class="text-white">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($contacts as $contact)
+            <tr>
+                <td>{{ $contact->name }}</td>
+                <td>{{ $contact->email }}</td>
+                <td>{{ $contact->subject }}</td>
+                @if(!($domain_page ?? false))
+                    <td>
+                        @if($contact->domain !== null)
+                            <a href="{{ route('admin.domains.show', [$contact->domain]) }}">
+                                <span class="badge badge-primary">{{ $contact->domain->name }}</span>
+                            </a>
+                        @endif
+                    </td>
+                @endif
+                <td class="text-right">
+                    <a class="btn btn-primary btn-sm" title="Détails"
+                       href="{{ route('admin.contacts.show', [$contact]) }}">
+                        <i class="mdi mdi-eye-outline"></i>
+                    </a>
+                    <button class="btn btn-danger btn-sm" data-toggle="modal" title="Supprimer"
+                            data-target="#delete-modal-{{ $contact->id }}">
+                        <i class="mdi mdi-trash-can-outline"></i>
+                    </button>
+                </td>
+                @component('components.delete-modal', [
+                    'id' => 'delete-modal-' . $contact->id,
+                    'title' => 'Supprimer le méssage de ' . $contact->name,
+                    'message' => 'Vous ne pourrez plus consulter ce méssage, êtes vous sûr?',
+                    'route' => route('admin.contacts.destroy', [$contact])
+                ])
+                @endcomponent
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
